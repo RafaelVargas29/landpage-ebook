c8fa95f7b453a9c6849e524113e7e5903513ebf8
Rafael Vargas <rafaelrgsv@gmail.com>
2023-06-23 13:13:45 -0300

    wip

--- src/components/Testimony.tsx
+++ src/components/Testimony.tsx
@@ -118,12 +118,14 @@ export default function Testimony () {
                 </div>
             </div>
         </div>
-        <div className="hidden lg:blockflex justify-starte w-full py-2 gap-2">
-            <a href="#item1" className="btn btn-xs">1</a> 
-            <a href="#item2" className="btn btn-xs">2</a> 
-            <a href="#item3" className="btn btn-xs">3</a> 
-            <a href="#item4" className="btn btn-xs">4</a>
-        </div> 
+        <div className='hidden lg:block'>
+            <div className="flex justify-start py-2 gap-2">
+                <a href="#item1" className="btn btn-xs">1</a> 
+                <a href="#item2" className="btn btn-xs">2</a> 
+                <a href="#item3" className="btn btn-xs">3</a> 
+                <a href="#item4" className="btn btn-xs">4</a>
+            </div>
+        </div>
         </>
     )
 }
