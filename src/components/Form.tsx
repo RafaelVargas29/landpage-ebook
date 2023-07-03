d917d0146c1513d5996a329853ef0eea11f06a47
Rafael Vargas <rafaelrgsv@gmail.com>
2023-07-03 16:38:25 -0300

    fix

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -50,7 +50,7 @@ export default function Form() {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'api-key': process.env.NEXT_PUBLIC_API_KEY,  
+        'api-key': process.env.NEXT_PUBLIC_API_KEY || '',  
       },
       body: JSON.stringify({ 
         email: data.email,
